87f1094240f2abb01e0c5b1a1f8d7a75733d8bd2
Vero Celemin <veronica.celemin@gmail.com>
2019-02-06 12:00:45 +0100

    fix: files-folders reorganized - routes fixed

--- controllers/events.controller.js
+++ controllers/events.controller.js
@@ -4,3 +4,21 @@ module.exports.viewEvent = (req, res, next) => {
   res.render('events/event');
 }
 
+module.exports.createEvent = (req, res, next) => {
+  res.render('events/create-event');
+}
+
+module.exports.doCreateEvent = (req, res, next) => {
+  console.log(req.body);
+  const eventBody = req.body;
+  console.log('file', req.file);
+  console.log('files', req.files);
+  if ( req.file ) {
+    eventBody.picture = req.file.secure_url;
+  }
+  console.log({ eventBody });
+  const event = new Event(eventBody);
+
+  event.save()
+    .then((event) => { res.redirect('/events/event')});
+}
--- routes/events.routes.js
+++ routes/events.routes.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const eventsController = require('../controllers/events.controller');
- 
+const userMiddleware = require('../middlewares/user.middlewares');
+const upload = require('../configs/multer.config');
+
 router.get('/event', eventsController.viewEvent); 
+router.get('/create-event', userMiddleware.isAuthenticated,eventsController.createEvent);
+router.post('/create-event',
+  userMiddleware.isAuthenticated,
+  upload.single('picture'),
+  eventsController.doCreateEvent
+);
 
 module.exports = router;
